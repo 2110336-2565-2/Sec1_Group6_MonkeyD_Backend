23e7ca254aba3a693b067947513cc95a64e9139f
Earth <kantapon.nate@gmail.com>
2023-02-05 03:06:16 +0700

    add car model and create car API

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,5 +1,54 @@
 import express from "express";
 import mongoose from "mongoose";
+import Car from "../models/car.model.js";
+
+export const createCars = (req, res, next) => {
+  console.log(req.body);
+  const car = new Car();
+  const {
+    owner,
+    status,
+    brand,
+    model,
+    age,
+    gear_type,
+    registration_book_id,
+    registration_book_url,
+    energy_types,
+    latitude,
+    longitude,
+    available_times,
+    rental_price,
+    car_images,
+  } = req.body.car;
+  car.owner = owner;
+  car.status = status;
+  car.brand = brand;
+  car.model = model;
+  car.age = age;
+  car.gear_type = gear_type;
+  car.registration_book_id = registration_book_id;
+  car.registration_book_url = registration_book_url;
+  car.energy_types = energy_types;
+  car.rental_price = rental_price;
+  car.car_images = car_images;
+  car.setLocation(latitude, longitude);
+  car.setAvailableTimes(available_times);
+
+  car
+    .save()
+    .then(function () {
+      return res.json({ car: car.toAuthJSON() });
+    })
+    .catch(function (error) {
+      if (error.code === 11000) {
+        return res.status(400).send({
+          error: "registration_book_id or registration_book_url already exists",
+        });
+      }
+      next(error);
+    });
+};
 
 export const getCars = (req, res, next) => {
   res.send("get cars");
---
+++ models/car.model.js
@@ -0,0 +1,142 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const CarSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: String,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true,
+    },
+    status: {
+      type: String,
+      required: [true, "can't be blank"],
+      enum: [
+        "Pending",
+        "Verified",
+        "Updating",
+        "Unavailable",
+        "Available",
+        "Rented",
+        "WaitForInspectation",
+        "Inspected",
+        "FixingOrPreparing",
+        "CarChecking",
+      ],
+    },
+    brand: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    model: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    gear_type: {
+      type: String,
+      enum: ["Manual", "Auto"],
+      required: [true, "can't be blank"],
+    },
+    age: {
+      type: Number,
+      required: [true, "can't be blank"],
+    },
+    registration_book_id: {
+      type: String,
+      unique: true,
+      required: [true, "can't be blank"],
+    },
+    registration_book_url: {
+      type: String,
+      unique: true,
+      required: [true, "can't be blank"],
+    },
+    energy_types: [
+      {
+        type: String,
+        enum: [
+          "DieselB7",
+          "DieselB10",
+          "Gasohol95",
+          "Gasohol91",
+          "E20",
+          "E85",
+          "LPG",
+          "NGV",
+          "EV",
+        ],
+        required: [true, "can't be blank"],
+      },
+    ],
+    location: {
+      //GeoJSON
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: [true, "can't be blank"],
+      },
+      coordinates: {
+        type: [Number], //[long,lat]
+        required: [true, "can't be blank"],
+      },
+    },
+    available_times: [
+      {
+        start: {
+          type: Date,
+          required: [true, "can't be blank"],
+        },
+        end: {
+          type: Date,
+          required: [true, "can't be blank"],
+        },
+      },
+    ],
+    rental_price: {
+      type: mongoose.Types.Decimal128,
+      required: [true, "can't be blank"],
+    },
+    car_images: [
+      {
+        type: String,
+      },
+    ],
+  },
+
+  { timestamps: true }
+);
+
+CarSchema.methods.setLocation = function (latitude, longitude) {
+  this.location = {
+    type: "Point",
+    coordinates: [longitude, latitude],
+  };
+};
+
+CarSchema.methods.setAvailableTimes = function (times) {
+  let available_times = times;
+  available_times.map(strToDate);
+  function strToDate(x) {
+    return {
+      start: new Date(x.start),
+      end: new Date(x.end),
+    };
+  }
+  this.available_times = available_times;
+};
+
+CarSchema.methods.toAuthJSON = function () {
+  return {
+    owner: this.owner,
+    brand: this.brand,
+    model: this.model,
+    registration_book_url: this.registration_book_url,
+  };
+};
+
+const Car = mongoose.model("Car", CarSchema);
+export default Car;
