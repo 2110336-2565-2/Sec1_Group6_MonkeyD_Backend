8425798ff1ff1c0a5f7c97b26d5096fbd8899880
PawanK <tutor_pawan@hotmail.com>
2023-02-08 21:29:08 +0700

    add jwt function

---
+++ middlewares/jwt.middleware.js
@@ -0,0 +1,34 @@
+import jwt from "express-jwt";
+
+dotenv.config({path: ".env"});
+
+const secret = process.env.JWT_SECRET;
+
+const getTokenFromHeader = (req) => {
+  if (
+    (req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Token") ||
+    (req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer")
+  ) {
+    return req.headers.authorization.split(" ")[1];
+  }
+
+  return null;
+};
+
+var auth = {
+  required: jwt({
+    secret: secret,
+    userProperty: "payload",
+    getToken: getTokenFromHeader,
+  }),
+  optional: jwt({
+    secret: secret,
+    userProperty: "payload",
+    credentialsRequired: false,
+    getToken: getTokenFromHeader,
+  }),
+};
+
+export default auth;
