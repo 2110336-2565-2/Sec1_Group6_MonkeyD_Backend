211dbda106251cf634ec96f9d074dd6e26779dc4
PawanK <tutor_pawan@hotmail.com>
2023-02-08 22:02:32 +0700

    add model

--- models/car.model.js
+++ models/car.model.js
@@ -15,7 +15,6 @@ const CarSchema = new mongoose.Schema(
     renter: {
       type: String,
       lowercase: true,
-      required: [true, "can't be blank"],
       match: [/^[a-zA-Z0-9]+$/, "is invalid"],
     },
     status: {
