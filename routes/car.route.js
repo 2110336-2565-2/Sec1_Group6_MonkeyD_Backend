a90cbf55436ff996f7bea9ba6fcebee9ba6ea0b4
PawanK <tutor_pawan@hotmail.com>
2023-02-08 21:59:58 +0700

    cb

--- routes/car.route.js
+++ routes/car.route.js
@@ -5,6 +5,6 @@ import auth from "../middlewares/jwt.middleware.js";
 const router = express.Router();
 
 router.route("/car").post(createCars);
-router.route("/car").get(getCars);
+router.route("/car").get(auth.required, getCars);
 
 export default router;
