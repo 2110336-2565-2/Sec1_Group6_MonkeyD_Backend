933b95cbe8bb6855fbf2aef16c26628bd73d0c9b
PawanK <tutor_pawan@hotmail.com>
2023-02-15 13:11:26 +0700

    fix route

--- routes/car.route.js
+++ routes/car.route.js
@@ -1,13 +1,17 @@
 import express from "express";
-import {createCars, getCars, getCarInfo, toggleRented} from "../controllers/car.controller.js";
+import {
+  createCars,
+  getCars,
+  getCarInfo,
+  toggleRented,
+} from "../controllers/car.controller.js";
 import auth from "../middlewares/jwt.middleware.js";
 
 const router = express.Router();
 
 router.route("/car").get(getCars);
 router.route("/car").post(auth.required, createCars);
-router.route("/car").get(auth.required, getCars);
-router.route("/car/:id").get(auth.required, getCarInfo);
+router.route("/car/:id").get(getCarInfo);
 router.route("/car").patch(auth.required, toggleRented);
 
 export default router;
