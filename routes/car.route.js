45dc7664b042dae9af08fc47025ed94d3eef7b4a
Permrawee <permrawee@gmail.com>
2023-03-13 12:59:45 +0700

    fix

--- routes/car.route.js
+++ routes/car.route.js
@@ -15,15 +15,13 @@ const router = express.Router();
 router
   .route("/car")
   .get(getCars)
-  .post(auth.required, createCars)
+  .post(createCars)
+  //.post(auth.required, createCars)
   .patch(auth.required, toggleRented)
   .delete(auth.required, deleteCar);
 router.route("/car/:id").get(getCarInfo);
-<<<<<<< HEAD
 router.route("/car/me/:username").get(getMyCar);
-=======
-router.route("/car/me/:username").get(auth.required, getMyCar);
+//router.route("/car/me/:username").get(auth.required, getMyCar);
 router.route("/car/change-car-info").patch(changeCarInfo);
->>>>>>> main
 
 export default router;
