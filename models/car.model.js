3cbf86ccc630466c5c682b44c4b22fc2ae025512
Kantaponn <78014536+Kantaponn@users.noreply.github.com>
2023-04-04 01:14:14 +0700

    delete status rented of car

--- models/car.model.js
+++ models/car.model.js
@@ -21,7 +21,7 @@ const CarSchema = new mongoose.Schema(
     status: {
       type: String,
       required: [true, "can't be blank"],
-      enum: ["Pending", "Rejected", "Unavailable", "Available", "Rented"],
+      enum: ["Pending", "Rejected", "Unavailable", "Available"],
       default: "Pending",
     },
     brand: {
