ea1a61e1be8732dc2d7a85c2b99f0a8e9abe07a6
Permrawee <permrawee@gmail.com>
2023-04-15 00:26:27 +0700

    swagger

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,3 +1,73 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Chat:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         matchID:
+ *           type: string
+ *           description: The ID of the match associated with the chat room.
+ *         allowedUsers:
+ *           type: array
+ *           items:
+ *             type: string
+ *             description: The IDs of the users allowed to access the chat room.
+ *       required:
+ *         - matchID
+ */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Message:
+ *       type: object
+ *       properties:
+ *         chatId:
+ *           type: string
+ *         text:
+ *           type: string
+ *         user:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *       required:
+ *         - chatId
+ *         - text
+ *         - user
+ *         - createdAt
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Chat
+ *   description: API endpoints for managing chat rooms
+ *
+ * /chat:
+ *   post:
+ *     summary: Create a new chat room
+ *     tags: [Chat]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ChatRoom'
+ *     responses:
+ *       201:
+ *         description: Returns the newly created chat room
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ChatRoom'
+ *       400:
+ *         description: Invalid or missing parameters in the request.
+ */
+
+
 import express from "express";
 import {authenticateUser} from "../middlewares/auth.middleware.js";
 import {createChat} from "../controllers/chat.controller.js";
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from "express";
+import csrf from "csrf";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {customLogger} from "./middlewares/logger.middleware.js";
-import {customCORS} from "./middlewares/cors.middleware.js";
-import {errorHandler} from "./middlewares/error-handler.middleware.js";
+import { customLogger } from "./middlewares/logger.middleware.js";
+import { customCORS } from "./middlewares/cors.middleware.js";
+import { errorHandler } from "./middlewares/error-handler.middleware.js";
 import routes from "./routes/index.js";
 import "./configs/passport.config.js";
 import http from "http";
 import connectToDatabase from "./configs/mongodb.config.js";
 import configureChatSocket from "./configs/chatSocket.config.js";
+
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 dotenv.config({path: ".env"});
 const isProduction = process.env.NODE_ENV === "production";
@@ -35,3 +39,20 @@ server.listen(
   PORT,
   console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
 );
+
+// Swagger documentation setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express VacQ API'
+    }
+  },
+  apis: ['./routes/*.js'],
+};
+
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
